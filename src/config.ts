d95af6b9359d3094c528f99d21830f5bc8f765ab
Rodrigo Boet <rudmanmrrod@gmail.com>
2024-05-29 18:43:10 -0400

    Remove settings

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyALbGCM7hSonF4GVU-QjVrRafqJENoX25k",
-  authDomain: "atomtest-75339.firebaseapp.com",
-  projectId: "atomtest-75339",
-  storageBucket: "atomtest-75339.appspot.com",
-  messagingSenderId: "150563149043",
-  appId: "1:150563149043:web:880339fd3c2d4a947bd386",
-  measurementId: "G-NREK0828ZS"
+  
 };
 
 export const ApiList: string[] = [
